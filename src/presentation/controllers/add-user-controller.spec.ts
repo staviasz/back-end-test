ee5953d87045dfb4cd084337e3d104a85fedd0db
codedbylucas <lucasmsantos819@gmail.com>
2024-02-03 17:43:32 -0300

    test: ensure AddUserController return 400 if email field is not provided

--- src/presentation/controllers/add-user-controller.spec.ts
+++ src/presentation/controllers/add-user-controller.spec.ts
@@ -5,9 +5,26 @@ describe('AddUserController', () => {
   it('Should return 400 if name field is not provided', async () => {
     const sut = new AddUserController();
     const httpResponse = await sut.execute({
-      body: { value: 'any_value' },
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
     });
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new RequiredFieldError('The name field is required'));
+  });
+
+  it('Should return 400 if email field is not provided', async () => {
+    const sut = new AddUserController();
+    const httpResponse = await sut.execute({
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    });
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new RequiredFieldError('The email field is required'));
   });
 });
