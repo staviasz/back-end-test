78c129d1a8db811bd823e7233f307552040e5854
codedbylucas <lucasmsantos819@gmail.com>
2024-02-03 17:01:36 -0300

    feat: ensure AddAccountUseCase call Hasher with correct password

--- src/usecases/implementations/add-account-usecase.ts
+++ src/usecases/implementations/add-account-usecase.ts
@@ -1,14 +1,19 @@
 import { AddAccount, AddAccountData } from 'src/domain/protocols/add-account';
 import { LoadAccountByEmailRepository } from '../protocols/db/load-account-by-email-repository';
+import { Hasher } from '../protocols/cryptography/hasher';
 
 export class AddAccountUseCase implements AddAccount {
-  constructor(private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+  constructor(
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hasher: Hasher,
+  ) {}
 
   async perform(data: AddAccountData): Promise<Error | string> {
     const loadAccountResult = await this.loadAccountByEmailRepository.loadByEmail(data.email);
     if (loadAccountResult) {
       return new Error(`Email '${data.email}' is invalid`);
     }
+    await this.hasher.hash(data.password);
     return '';
   }
 }
