497fe91e9fa3c772034df61c084187d792eb8fec
codedbylucas <lucasmsantos819@gmail.com>
2024-02-04 14:16:18 -0300

    feat: load an Account by email

--- src/infra/db/account-prisma-repository.ts
+++ src/infra/db/account-prisma-repository.ts
@@ -1,8 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 import { AccountModel } from 'src/domain/models/account';
 import { AddAccountRepository, AddAccountRepositoryData } from 'src/usecases/protocols/db/add-account-repository';
+import { LoadAccountByEmailRepository } from 'src/usecases/protocols/db/load-account-by-email-repository';
 
-export class AccountPrismaRepository implements AddAccountRepository {
+export class AccountPrismaRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   constructor(private readonly prisma: PrismaClient) {}
 
   async add(data: AddAccountRepositoryData): Promise<AccountModel> {
@@ -17,5 +18,10 @@ export class AccountPrismaRepository implements AddAccountRepository {
       },
     });
     return account;
+  }
+
+  async loadByEmail(email: string): Promise<null | AccountModel> {
+    const accountOrNull = await this.prisma.account.findUnique({ where: { email } });
+    return accountOrNull;
   }
 }
