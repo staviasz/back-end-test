ccae103524d0fa24883653b26cc5d535d10f44bf
codedbylucas <lucasmsantos819@gmail.com>
2024-02-04 14:25:02 -0300

    feat: add NestRouteAdapter

---
+++ src/main/adapters/nest-route-adapter.ts
@@ -0,0 +1,23 @@
+import type { Controller } from 'src/presentation/contracts/controller';
+import { type HttpRequest } from 'src/presentation/types/http';
+import type { Response, Request } from 'express';
+
+export class NestRouteAdapter {
+  constructor(private readonly controller: Controller) {}
+
+  async adapt(req: Request, res: Response): Promise<void> {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+    };
+    const httpResponse = await this.controller.execute(httpRequest);
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+      res.status(httpResponse.statusCode).json(httpResponse.body);
+    } else {
+      res.status(httpResponse.statusCode).json({
+        name: httpResponse.body.name,
+        error: httpResponse.body.message,
+        statusCode: httpResponse.statusCode,
+      });
+    }
+  }
+}
